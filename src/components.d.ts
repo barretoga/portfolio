bd97bd97b11cd11c4549c8736c480e291ef3d6f7
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-13 21:15:56 -0300

    ✨ feat: add badge component to components global file

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Badges: typeof import('./components/Badges.vue')['default']
     Image: typeof import('./components/Image.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']

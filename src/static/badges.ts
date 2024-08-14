8543a7e18e2bb785b6f3978b93cc041cf736acfb
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-14 20:21:27 -0300

    ✨ feat: create badges static file

---
+++ src/static/badges.ts
@@ -0,0 +1,67 @@
+interface BadgeItem {
+  name: string
+  image_path: string
+}
+
+export const badgeItems: BadgeItem[] = [
+  {
+    name: 'JavaScript',
+    image_path: '/src/assets/images/js-logo.png'
+  },
+  {
+    name: 'TypeScript',
+    image_path: '/src/assets/images/ts-logo.png'
+  },
+  {
+    name: 'Vue',
+    image_path: '/src/assets/images/vue-logo.png'
+  },
+  {
+    name: 'Nuxt',
+    image_path: '/src/assets/images/nuxt-logo.png'
+  },
+  {
+    name: 'React',
+    image_path: '/src/assets/images/react-logo.png'
+  },
+  {
+    name: 'Astro',
+    image_path: '/src/assets/images/astro-logo.png'
+  },
+  {
+    name: 'Svelte',
+    image_path: '/src/assets/images/svelte-logo.png'
+  },
+  {
+    name: 'Angular',
+    image_path: '/src/assets/images/angular-logo.png'
+  },
+  {
+    name: 'Cypress',
+    image_path: '/src/assets/images/cypress-logo.png'
+  },
+  {
+    name: 'Vite/Vitest',
+    image_path: '/src/assets/images/vite-logo.png'
+  },
+  {
+    name: 'Java',
+    image_path: '/src/assets/images/java-logo.png'
+  },
+  {
+    name: 'Springboot',
+    image_path: '/src/assets/images/springboot-logo.png'
+  },
+  {
+    name: 'Python',
+    image_path: '/src/assets/images/python-logo.png'
+  },
+  {
+    name: 'PHP',
+    image_path: '/src/assets/images/php-logo.png'
+  },
+  {
+    name: 'Figma',
+    image_path: '/src/assets/images/figma-logo.png'
+  }
+]

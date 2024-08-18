c3866f18fb386b6b19c8162730689e51d6551377
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-18 17:05:40 -0300

    💄 style: add responsiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,9 +13,9 @@ const showCompleteDescription = ref(false)
       type="video/webm"
     />
     <div
-      class="flex flex-col bg-gradient-to-br from-gradient-background via-gradient-background to-gradient-showcase-header-left rounded-lg max-w-[976px] w-full lg:w-[976px] z-[4] relative"
+      class="flex flex-col bg-gradient-to-br overflow-x-hidden from-gradient-background via-gradient-background to-gradient-showcase-header-left rounded-lg max-w-[976px] w-full lg:w-[976px] z-[4] relative"
     >
-      <div class="flex py-8 px-6">
+      <div class="flex flex-col lg:flex-row items-center py-8 px-6">
         <div class="relative w-full max-w-[200px]">
           <Image
             class="w-[164px] h-[164px] border-2 border-sky-300"
@@ -28,7 +28,7 @@ const showCompleteDescription = ref(false)
             alt="Moldura do perfil"
           />
         </div>
-        <div class="flex flex-col w-full max-w-[450px] pr-5 text-sm">
+        <div class="flex flex-col w-full max-w-[90%] lg:max-w-[450px] lg:pr-5 lg:pt-0 pt-5 text-sm">
           <h1 class="text-2xl">barreto</h1>
           <div class="flex mb-5">
             <span
@@ -69,7 +69,7 @@ const showCompleteDescription = ref(false)
             {{ showCompleteDescription ? 'Ver menos informações' : 'Ver mais informações' }}
           </button>
         </div>
-        <div class="flex flex-col w-full max-w-[268px] col-span-2">
+        <div class="flex flex-row lg:justify-normal justify-between lg:flex-col w-full max-w-[90%] lg:max-w-[268px] col-span-2">
           <div class="flex items-center">
             <h1
               class="mr-2 text-xl"
@@ -99,9 +99,9 @@ const showCompleteDescription = ref(false)
           </div>
         </div>
       </div>
-      <div class="flex">
-        <div class="w-full max-w-[652px] px-3 ml-2 mb-4">
-          <div class="bg-box-background rounded">
+      <div class="flex lg:flex-row flex-col items-center lg:items-start">
+        <div class="w-full max-w-[90%] lg:mx-0 lg:max-w-[652px] lg:px-3 lg:ml-2 mb-4">
+          <div class="bg-box-background rounded mx-2 lg:mx-0">
             <div class="flex bg-gradient-to-r from-gradient-showcase-header-left to-color-showcase-header rounded-t-md px-3 pt-1">
               <h2 class="text-lg mb-2 mr-2">
                 Formação
@@ -121,7 +121,7 @@ const showCompleteDescription = ref(false)
               Sou formado pela Faculdade de Tecnologia de São Paulo - FATEC Jahú em Gestão da Tecnologia da Informação (02/2020 - 12/2023).
             </p>
           </div>
-          <div class="bg-box-background rounded mt-5">
+          <div class="bg-box-background lg:mx-0 mx-2 rounded mt-5">
             <div class="flex bg-gradient-to-r from-gradient-showcase-header-left to-color-showcase-header rounded-t-md px-3 pt-1">
               <h2 class="text-lg mb-2 mr-2">
                 Projetos
@@ -137,81 +137,81 @@ const showCompleteDescription = ref(false)
                 />
               </a>
             </div>
-            <div class="grid grid-cols-5 gap-2 p-2">
+            <div class="grid items-center lg:items-start lg:grid-cols-5 gap-1 lg:gap-2 p-2">
               <a
-                class="col-span-4 w-[486px] h-[334px] hover:border-gray-50 border border-transparent transition-all duration-200 overflow-hidden"
+                class="lg:col-span-4 lg:w-[486px] lg:h-[334px] w-[100%] h-[150px] lg:mx-0 mx-auto hover:border-gray-50 border border-transparent transition-all duration-200 overflow-hidden"
                 href="https://barretodev.vercel.app/"
                 target="_blank"
               >
                 <Image
-                  class="w-[506px] h-[334px] hover:scale-110 transition-all duration-200 object-cover"
+                  class="lg:w-[506px] lg:h-[334px] w-[100%] h-[150px] hover:scale-110 transition-all duration-200 lg:object-cover"
                   src="blog.png"
                   alt="Blog"
                 />
               </a>
-              <div class="flex flex-col col-span-1 gap-y-1">
+              <div class="flex flex-col lg:items-start items-center w-full col-span-1 gap-y-1">
                 <a
-                  class="w-[116px] h-[80px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
+                  class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
                   href="https://commer-application.netlify.app/"
                   target="_blank"
                 >
                   <Image
-                    class="w-[116px] h-[80px] hover:scale-110 transition-all duration-200 object-cover"
+                    class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:scale-110 transition-all duration-200 object-cover"
                     src="commer.png"
                     alt="Commer"
                   />
-                  <div class="w-[116px] h-[80px] top-0 bg-yellow-700/70 absolute opacity-0 hover:opacity-100 transition-all duration-200">
-                    <span class="text-xs text-center w-full pl-3 font-semibold">
+                  <div class="lg:w-[116px] lg:items-start lg:pt-1 pt-0 flex flex-col items-center justify-center lg:justify-normal lg:h-[80px] w-[100%] h-[150px] top-0 bg-yellow-700/70 absolute opacity-0 hover:opacity-100 transition-all duration-200">
+                    <span class="text-xs text-center w-full font-semibold">
                       Work in progress
                     </span>
                     <Icon
-                      class="mx-auto"
+                      class="mx-auto lg:mt-0 mt-2"
                       icon="noto:construction"
                       width="3em"
                     />
                   </div>
                 </a>
                 <a
-                  class="w-[116px] h-[80px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
+                  class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
                   href="https://github.com/barretoga/commer-api"
                   target="_blank"
                 >
                   <Image
-                    class="w-[116px] h-[80px] hover:scale-110 transition-all duration-200 object-cover"
+                    class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:scale-110 transition-all duration-200 object-cover"
                     src="commer.png"
                     alt="Commer API"
                   />
-                  <div class="absolute w-[116px] h-[80px] text-black/80 bg-gray-300/50 font-bold text-xl top-0">
+                  <div class="absolute lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] text-black/80 bg-gray-300/50 font-bold text-xl top-0">
                     <Icon
-                      class="mx-auto mt-1"
+                      class="mx-auto lg:mt-1 mt-9"
                       icon="tabler:database-cog"
                       width="3em"
                     />
                   </div>
-                  <div class="w-[116px] h-[80px] top-0 bg-yellow-700/70 absolute opacity-0 hover:opacity-100 transition-all duration-200">
-                    <span class="text-xs text-center w-full pl-3 font-semibold">
+                  <div class="lg:w-[116px] lg:items-start lg:pt-1 pt-0 flex flex-col items-center justify-center lg:justify-normal lg:h-[80px] w-[100%] h-[150px] top-0 bg-yellow-700/70 absolute opacity-0 hover:opacity-100 transition-all duration-200">
+                    <span class="text-xs text-center w-full font-semibold">
                       Work in progress
                     </span>
                     <Icon
-                      class="mx-auto"
+                      class="mx-auto lg:mt-0 mt-5"
                       icon="noto:construction"
                       width="3em"
                     />
                   </div>
                 </a>
                 <a
-                  class="w-[116px] h-[80px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
+                  class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:border-gray-50 border border-transparent relative transition-all duration-200 overflow-hidden"
                   href="https://github.com/barretoga/nlw-2024-planner"
                   target="_blank"
                 >
                   <Image
-                    class="w-[116px] h-[80px] hover:scale-110 transition-all duration-200 object-cover"
+                    class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] hover:scale-110 transition-all duration-200 object-cover"
                     src="nlw.png"
                     alt="NLW Planner"
                   />
                 </a>
                 <a
-                  class="w-[116px] h-[80px] bg-[#222223] hover:border-gray-50 border border-transparent flex items-center transition-all duration-200 overflow-hidden"
+                  class="lg:w-[116px] lg:h-[80px] w-[100%] h-[150px] bg-[#222223] hover:border-gray-50 border border-transparent flex items-center transition-all duration-200 overflow-hidden"
                   href="https://github.com/barretoga?tab=repositories"
                   target="_blank"
                 >
@@ -223,7 +223,7 @@ const showCompleteDescription = ref(false)
             </div>
           </div>
         </div>
-        <div class="flex flex-col w-full max-w-[288px] bg-box-background rounded-md p-3 ml-4 mr-4 mb-4">
+        <div class="flex flex-col w-full max-w-[85%] bg-box-background rounded-md p-3 ml-0 mr-0 lg:ml-4 lg:mr-4 mb-4 lg:max-w-[288px]">
           <span class="text-xl text-[#57CDBE]">
             Working
           </span>
